20fa469aec7324d2390dab882efbc066bb9a3641
ZeroPointGaming <comfyseven4@gmail.com>
2021-08-11 22:56:59 -0400

    Update diy-space-program.js

--- src/pages/irl/diy-space-program.js
+++ src/pages/irl/diy-space-program.js
@@ -1,1 +1,12 @@
-import * from React as react;
+// Step 1: Import your component
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+      <p>Coming Soon</p>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
